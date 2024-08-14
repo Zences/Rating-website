dad5a672fc663c6ecae9f2a1f15c84307f26e1e4
Zences <noahtonda11@gmail.com>
2024-08-14 11:25:01 +0300

    feat: logina nd register work witout sessions

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const express = require('express')
+const crypto = require('crypto');
 const pg =  require('pg')
 const { Pool, Client } = pg
 const app = express()
 const port = 3000
 require('dotenv').config()
+
+// Define a function to hash a password using SHA-1
+function hashPassword(password) {
+  // Create a SHA-1 hash object
+  const hash = crypto.createHash('sha1');
+  
+  // Update the hash with the password (encoded as a string)
+  hash.update(password);
+  
+  // Generate the hash digest in hexadecimal format
+  const hashedPassword = hash.digest('hex');
+  
+  return hashedPassword;
+}
 
 app.set('views', __dirname + '/templates');
 app.engine('html', require('ejs').renderFile);
@@ -26,6 +41,53 @@ app.get('/review', (req, res) => {
     res.render('review.html')
 })
 
+
+//This is the route to the login page
+app.get('/login', (req, res) => {
+  res.render('login.html')
+})
+
+app.post('/login', async (req, res) => {
+  const { email , password } = req.body;
+  const hashedPassword = hashPassword(password)
+
+  const query = `SELECT * FROM users WHERE username='${email}' AND password='${hashedPassword}' LIMIT 1`
+  try {
+    const response = await rundbquery(query);
+    if(response.rowCount==0){
+      res.status(401).send("User was not found");
+      return
+    }
+    res.redirect("/")
+    // res.status(200).send({error:'Login succesful'});
+  } catch (error) {
+    console.error('Error adding review:', error);
+    res.status(500).send({error});
+  }
+})
+
+//This is the route to the register page
+app.get('/register', (req, res) => {
+  res.render('register.html')
+})
+
+
+
+app.post('/register', async (req, res) => {
+  const { email , password } = req.body;
+  const hashedPassword = hashPassword(password)
+
+  const query = `INSERT INTO users (username,password) VALUES ('${email}','${hashedPassword}')`
+  try {
+    const response = await rundbquery(query);
+    res.redirect("/login")
+  } catch (error) {
+    console.error('Error adding review:', error);
+    res.status(500).send({error:'Failed to add user'});
+  }
+})
+
+//THis route handles saving of reviews to the database
 app.post('/review', async (req, res) => {
   // console.log(req.body)
   const { username , review, imdb_id } = req.body;
---
+++ readme.md
@@ -0,0 +1,5 @@
+## Movie review app
+
+TO run this app, you need to have nodejs installed and follow the instructions that follow.
+- Install the required libraries using `npm i`
+- To run the project, simply use the command `node --watch app.js` for development and `node app.js` for deployment or use a process manager.
